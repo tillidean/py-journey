06c660c950422fef6ed6c5fce62b0f8d0a65f7ff
JoWanTech <jowansulaiman@gmail.com>
2021-12-01 21:04:58 +0100

    Initial commit

---
+++ Sort/QuickSort.cpp
@@ -0,0 +1,18 @@
+//
+// Created by JoWan on 01.12.2021.
+//
+
+#include "QuickSort.h"
+
+void QuickSort::swapp(int i, int j, int *arr) {
+
+}
+
+void QuickSort::partation(int i, int j, int pt) {
+
+}
+
+void QuickSort::sort(int *arr) {
+
+}
+
